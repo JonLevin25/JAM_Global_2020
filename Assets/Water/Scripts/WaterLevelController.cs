f570cad9d5af1a2093a84d77c288c6353486f94a
Jonathan Levin <jonmlevin@outlook.com>
2020-01-31 00:05:01 +0200

    Water level rising

---
+++ Assets/Water/Scripts/WaterLevelController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class WaterLevelController : MonoBehaviour
+{
+    [SerializeField] private float _startWaterLevel;
+    [SerializeField] private float _smoothTime;
+    [SerializeField] private WaterLevelView _water;
+
+    [SerializeField] private bool _TEST_setWater;
+    [SerializeField] private float _TEST_targetWaterLevel;
+
+    private float _currWaterLevel;
+    private float _targetWaterLevel;
+    private float _velocity;
+
+    private void Start()
+    {
+        _targetWaterLevel = _startWaterLevel;
+    }
+    
+    private void Update()
+    {
+        // Debugging/Testing
+        if (_TEST_setWater)
+        {
+            _targetWaterLevel = _TEST_targetWaterLevel;
+        }
+        
+        // Smooth water to level
+        _currWaterLevel = Mathf.SmoothDamp(_currWaterLevel, _targetWaterLevel, ref _velocity, _smoothTime);
+        _water.SetWaterLevel(_currWaterLevel);
+    }
+
+    public void IncreaseWaterLevel(float delta)
+    {
+        _targetWaterLevel += delta;
+    }
+}
