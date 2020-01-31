e174290902a333227a69c54cdb33ee1d2b85ac47
Jonathan Levin <jonmlevin@outlook.com>
2020-01-31 13:04:41 +0200

    GameOverCanvas

---
+++ Assets/UI/GameOverController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    [SerializeField] private Button gameOverButton;
+    [SerializeField] private int restartSceneIndex;
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        EventSystem.current.SetSelectedGameObject(gameOverButton.gameObject);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(restartSceneIndex);
+    }
+}
