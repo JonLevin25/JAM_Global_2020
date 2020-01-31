97798b06bd373d3bd7c3f9b16a2f487f44478844
Jonathan Levin <jonmlevin@outlook.com>
2020-01-31 22:17:26 +0200

    WaterLevelController

--- Assets/Water/Scripts/WaterLevelController.cs
+++ Assets/Water/Scripts/WaterLevelController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class WaterLevelController : MonoBehaviour
 {
@@ -14,9 +15,20 @@ public class WaterLevelController : MonoBehaviour
     private float _currWaterLevel;
     private float _targetWaterLevel;
     private float _velocity;
+    private int _prevHighestFloodedFloor = -1;
 
-    public int FloodedFloorCount => Mathf.FloorToInt(FloorHelper.Instance.GetFloor(_currWaterLevel));
+    public int HighestFloodedFloor
+    {
+        get
+        {
+            var waterLevel = FloorHelper.Instance.GetFloor(_currWaterLevel);
+            return Mathf.FloorToInt(waterLevel - 0.5f); // Return the last floor who'se more than half flooded
+        }
+    }
+
     public float CurrWaterLevel => _currWaterLevel;
+
+    public event Action<int> OnFloorFlooded;
 
     private void Awake()
     {
@@ -45,6 +57,15 @@ public class WaterLevelController : MonoBehaviour
         // Smooth water to level
         _currWaterLevel = Mathf.SmoothDamp(_currWaterLevel, _targetWaterLevel, ref _velocity, _smoothTime);
         _water.SetWaterLevel(_currWaterLevel);
+
+        if (HighestFloodedFloor > _prevHighestFloodedFloor)
+        {
+            var newFloorFlooded = _prevHighestFloodedFloor + 1;
+            OnFloorFlooded?.Invoke(newFloorFlooded);
+            
+            Debug.Log($"OnFloorFlooded({newFloorFlooded}) [WaterLevel: {_currWaterLevel}]");
+            _prevHighestFloodedFloor = newFloorFlooded;
+        }
     }
 
     public void IncreaseWaterLevel(float delta)
