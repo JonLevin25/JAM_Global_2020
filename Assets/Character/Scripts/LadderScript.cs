dce0cc1e297bd2ee3e02f599d8503ee6c55ec555
Omer Zadok <OZadok@gmail.com>
2020-01-31 02:32:17 +0200

    Ladder with automated OnBuild collider list

--- Assets/Character/Scripts/LadderScript.cs
+++ Assets/Character/Scripts/LadderScript.cs
@@ -1,10 +1,49 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
 
 namespace Character.Scripts
 {
+   [RequireComponent(typeof(BoxCollider2D))]
    public class LadderScript : MonoBehaviour
    {
       public List<Collider2D> groundCollider2Ds;
+   }
+   
+   class LadderCustomBuildProcessor : IProcessSceneWithReport
+   {
+      public int callbackOrder { get { return 0; } }
+      public void OnProcessScene(UnityEngine.SceneManagement.Scene scene, BuildReport report)
+      {
+         Debug.Log("LadderCustomBuildProcessor.OnProcessScene " + scene.name);
+
+         var ladders = GameObject.FindObjectsOfType<LadderScript>();
+         foreach (var ladder in ladders)
+         {
+            BoxCollider2D boxCollider = ladder.GetComponent<BoxCollider2D>();
+            var transform = ladder.transform;
+            Vector2 origin = (Vector2) transform.position + boxCollider.offset;
+            Vector2 size = boxCollider.size * (Vector2)transform.lossyScale;
+            RaycastHit2D[] results = Physics2D.BoxCastAll(origin, size, 0, Vector2.up, 0);
+
+            if (ladder.groundCollider2Ds == null)
+            {
+               ladder.groundCollider2Ds = new List<Collider2D>();
+            }
+
+            var ladderColliders = ladder.GetComponents<Collider2D>();
+            foreach (var result in results)
+            {
+               Collider2D collider = result.collider;
+               if (ladderColliders.Contains(collider))
+               {
+                  continue;
+               }
+               ladder.groundCollider2Ds.Add(collider);
+            }
+         }
+      }
    }
 }
