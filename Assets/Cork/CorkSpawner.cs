669f28d3a93588725cde0b37b479e6a1e94ed8f5
Jonathan Levin <jonmlevin@outlook.com>
2020-02-01 03:35:06 +0200

    bouncy cork

--- Assets/Cork/CorkSpawner.cs
+++ Assets/Cork/CorkSpawner.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class CorkSpawner : MonoBehaviour
 {
     [SerializeField] private Cork corkPrefab;
+    [SerializeField] private float randomForcePower;
+    [SerializeField] private Vector2 randomAngleRange;
 
     public void Spawn()
     {
         var cork = Instantiate(corkPrefab);
+        cork.transform.Rotate(new Vector3(0f, 0f, Random.Range(randomAngleRange[0], randomAngleRange[1])));
         cork.transform.position = transform.position;
+        
+        var randomForce = randomForcePower * Random.insideUnitCircle;
+        cork.rigidbody.AddForce(randomForce, ForceMode2D.Impulse);
     }
 }
