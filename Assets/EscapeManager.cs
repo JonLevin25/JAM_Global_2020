05c19154a7266e5cb4ad2337bee02e7e7addce16
Jonathan Levin <jon.levin25@gmail.com>
2020-02-02 02:30:14 +0200

    EscapeManager

---
+++ Assets/EscapeManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EscapeManager : MonoBehaviour
+{
+    [SerializeField] private bool _dontDestroyOnLoad;
+
+    private void Start()
+    {
+        if (_dontDestroyOnLoad) DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
